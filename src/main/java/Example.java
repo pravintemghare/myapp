86dd75610f3aeebe9e5b5fd726aa84d47b17f779
pravintemghare <47313488+pravintemghare@users.noreply.github.com>
2021-09-07 17:50:02 +0530

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!!!...";
+        return "Welcome to Kubernetes Cluster!!!...";
     }
 
     public static void main(String[] args) throws Exception {
