cdc8579cc529505542799741ee34102987ee6886
pravintemghare <47313488+pravintemghare@users.noreply.github.com>
2021-09-20 12:36:24 +0530

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "MiniKube Cluster!!!...";
+        return "MiniKube Cluster!!!";
     }
 
     public static void main(String[] args) throws Exception {
