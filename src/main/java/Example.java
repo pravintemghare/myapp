2b380de04013daed62946f7d2baea22c87a901cc
Pravin Temghare <pravin.a.temghare@capgemini.com>
2021-09-20 17:00:11 +0530

    test1

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Welcome to MiniKube Cluster!!";
+        return "Welcome to MiniKube Cluster!!!";
     }
 
     public static void main(String[] args) throws Exception {
